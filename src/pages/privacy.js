1cf9e8a45f7f5ecdbbb5b8ab63c3c5a8b6865a23
ArturKlajnerok <a.klajnerok@gmail.com>
2018-02-02 17:00:29 +0100

    Basic template pages for Privacy Policy and Terms of Use

---
+++ src/pages/privacy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const PrivacyPolicy = () => (
+  <div className="hero-body">
+    <div className="content">
+      <h1>Hi from the Privacy Page</h1>
+      <p>Welcome to Privacy Policy</p>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
+  </div>
+);
+
+export default PrivacyPolicy;
---
+++ src/pages/terms.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Link from 'gatsby-link';
+
+const TermsOfUse = () => (
+  <div className="hero-body">
+    <div className="content">
+      <h1>Hi from the Terms Page</h1>
+      <p>Welcome to Terms of Use</p>
+      <Link to="/">Go back to the homepage</Link>
+    </div>
+  </div>
+);
+
+export default TermsOfUse;

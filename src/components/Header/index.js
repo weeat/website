864b8d6dae5794d50fa4a7c04c74a1ccd50670b6
ArturKlajnerok <a.klajnerok@gmail.com>
2018-02-02 15:31:16 +0100

    Header with top fixed responsive navigation menu and burger menu for small screen

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'gatsby-link';
 
-const Header = () => (
-  <div
-    style={{
-      background: '#4CAF50',
-      marginBottom: '1.45rem'
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem'
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        >
-          WeEat
-        </Link>
-      </h1>
-    </div>
-  </div>
-);
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isBurgerActive: false };
+    this.handleBurgerClick = this.handleBurgerClick.bind(this);
+  }
+
+  handleBurgerClick() {
+    this.setState({ isBurgerActive: !this.state.isBurgerActive });
+  }
+
+  render() {
+    const burgerClassList = this.state.isBurgerActive
+      ? 'navbar-burger is-active'
+      : 'navbar-burger';
+    const menuClassList = this.state.isBurgerActive
+      ? 'navbar-menu is-active'
+      : 'navbar-menu';
+    return (
+      <nav
+        className="navbar is-fixed-top"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="navbar-brand">
+          <Link
+            to="/"
+            className="navbar-item has-text-primary has-text-weight-bold"
+          >
+            WeEat
+          </Link>
+
+          <span className={burgerClassList} onClick={this.handleBurgerClick}>
+            <span> </span>
+            <span> </span>
+            <span> </span>
+          </span>
+        </div>
+
+        <div className={menuClassList}>
+          <div className="navbar-end">
+            <a className="navbar-item">Home</a>
+            <a className="navbar-item">Features</a>
+            <a className="navbar-item">About</a>
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
 
 export default Header;

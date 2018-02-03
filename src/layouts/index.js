707ff8310ca0db8589b4161f9692a6fcf80f61c2
ArturKlajnerok <a.klajnerok@gmail.com>
2018-02-03 11:41:07 +0100

    Import scss in main layout and use footer component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 
-import Header from '../components/Header'
-import './index.css'
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+//import './index.css';
+import './index.scss';
 
 const TemplateWrapper = ({ children }) => (
-  <div>
+  <div className="has-navbar-fixed-top">
     <Helmet
       title="Gatsby Default Starter"
       meta={[
         { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'keywords', content: 'sample, something' }
       ]}
     />
+
     <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
+    <div>{children()}</div>
+    <Footer />
   </div>
-)
+);
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
-}
+  children: PropTypes.func
+};
 
-export default TemplateWrapper
+export default TemplateWrapper;

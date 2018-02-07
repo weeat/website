697bb19f59244667889d548969619becccc9451f
ArturKlajnerok <a.klajnerok@gmail.com>
2018-02-07 17:33:21 +0100

    New social section component for main page

---
+++ src/components/Main/SocialSection.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const SocialSection = () => (
+  <section id="social" className="hero">
+    <div className="hero-body">
+      <div className="container has-text-centered">
+        <div className="columns is-vcentered">
+          <div className="column">
+            <br />
+            <h1 className="title is-3 has-text-primary">
+              Get food tips from your friends, not strangers
+            </h1>
+            <br />
+            <h2 className="subtitle has-more-line-space">
+              Most valuable recommendations are from the people we know. Check
+              your friends activity and let them help you choose your next
+              delicious meal.
+            </h2>
+          </div>
+
+          <div className="column">
+            <div className="phone-frame-section">
+              <figure className="image">
+                <img
+                  src="img/screenshots/activity-screen-small.png"
+                  alt="WeEat phone screen"
+                  style={{
+                    height: '400px',
+                    width: '232px',
+                    marginTop: '60px'
+                  }}
+                />
+              </figure>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+export default SocialSection;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,50 +3,16 @@ import Link from 'gatsby-link';
 
 import ExploreSection from '../components/Main/ExploreSection';
 import IntroSection from '../components/Main/IntroSection';
+import SocialSection from '../components/Main/SocialSection';
 import VisualSection from '../components/Main/VisualSection';
 
 class IndexPage extends Component {
-  renderSocial() {
-    return (
-      <section id="social" className="hero">
-        <div className="hero-body">
-          <div className="container has-text-centered">
-            <div className="columns is-vcentered">
-              <div className="column">
-                <br />
-                <h1 className="title is-3">
-                  Get food tips from your friends, not strangers
-                </h1>
-                <br />
-                <h2 className="subtitle">
-                  Most valuable recommendations are from the people we know
-                  something about. Check your friends activity and let them help
-                  you choose your next delicious meal.
-                </h2>
-              </div>
-
-              <div className="column">
-                <figure className="image">
-                  <img
-                    src="img/phone.png"
-                    alt="WeEat phone screen"
-                    style={{ height: '500px', width: '245px' }}
-                  />
-                </figure>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  }
-
   render() {
     return (
       <div>
         <IntroSection />
         <ExploreSection />
-        {this.renderSocial()}
+        <SocialSection />
         <VisualSection />
       </div>
     );
